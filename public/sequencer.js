d5bcf8890643a4ac8959cb12bd5dd2439e277925
Gery Egan <gery.egan@gmail.com>
2016-10-11 11:30:52 -0700

    everything but animation complete

--- public/sequencer.js
+++ public/sequencer.js
@@ -1,3 +1,76 @@
+var sounds = [];
+var currentCol = 0;
+var playState = false;
+var targetLine = new Path.Line({
+    from: [20, 20],
+    to: [20, 835],
+    strokeColor: '#EB4511',
+    strokeWidth: 10
+})
+var destLine = new Path.Line({
+    from: [1228, 20],
+    to: [1228, 835],
+    strokeColor: '#EB4511',
+    strokeWidth: 10
+})
+
+var innerPlay;
+
+ setInterval(function(){
+				
+			for(var i = 0; i < sounds.length; i++){	
+
+				if(sounds[i].rowIndex === currentCol){
+						sounds[i].strokeColor= '#EB4511';
+					} else {
+						sounds[i].strokeColor= '#E1F2FE';
+					}
+				}
+			},10)
+console.log(sounds);
+var muteRow = function(sound){
+	for(var i = 0; i < sounds.length; i++){
+		if (sounds[i].sound === sound){
+			sounds[i].sound.mute = true;
+		}
+	}
+}
+var unmuteRow = function(sound){
+	for(var i = 0; i < sounds.length; i++){
+		if (sounds[i].sound === sound){
+			sounds[i].sound.mute = false;
+		}
+	}
+}
+var play = function(){
+
+	
+	innerPlay = setInterval(function(){
+				
+				for(var i = 0; i < sounds.length; i++){	
+
+					if(sounds[i].rowIndex === currentCol){
+						sounds[i].strokeColor= '#EB4511';
+					}
+					else{
+						sounds[i].strokeColor= '#E1F2FE';
+					}
+				
+				
+					if(sounds[i].rowIndex === currentCol && sounds[i].toggle){
+						
+						sounds[i].sound.play();
+					}
+				}
+				if (currentCol < 15){
+					currentCol++;
+				} else {currentCol = 0;}
+			}, 125);
+		
+	}
+var stop = function(){
+	clearInterval(innerPlay);
+}
 var kick = new Howl({
   src: ['sounds/bigkik.wav']
 });	
@@ -16,31 +89,38 @@ var chord = new Howl({
 var chordTwo = new Howl({
   src: ['sounds/chord2.wav']
 });
-var chordThree = new Howl({
-  src: ['sounds/chord3.wav']
-});
-var chordFour = new Howl({
-  src: ['sounds/chord4.wav']
+var bass = new Howl({
+  src: ['sounds/bass.wav']
 });
 
-var rowCreator = function(yCoord, sound){
+
+var rowCreator = function(yCoord, sound, row){
+	var rowIndex = 0;
 	for(var x = 35; x < 1235; x+= 75){
-
+		
 		var stepRect = new Path.Rectangle({
 			point: [x,yCoord],
 			size: [50,80.9],
 			fillColor: '#30323D',
 			strokeColor: '#E1F2FE',
+			strokeWidth: 3,
 			toggle: false,
-			sound: sound
-
-
+			sound: sound,
+			current: false,
+			rowIndex: rowIndex,
+			row: row
 		})
+		
+		sounds.push(stepRect);
+		rowIndex++;
 		stepRect.onClick = function(e){
 			if (this.toggle === false){
 				this.fillColor = '#ADFCF9';
 				this.toggle = true;
-				this.sound.play();
+				if(playState === false){
+					this.sound.play();
+				}
+				console.log(this.rowIndex);
 			} else {
 				this.fillColor = '#30323D';
 				this.toggle = false;
@@ -49,11 +129,49 @@ var rowCreator = function(yCoord, sound){
 	}
 }
 
-var kickRow = rowCreator(35, kick);
-var clapRow = rowCreator(135, clap);
-var hatRow = rowCreator(235, hat);
-var chordRow = rowCreator(335, chord);
+var kickRow = rowCreator(35, kick,0);
+var clapRow = rowCreator(135, clap,1);
+var hatRow = rowCreator(235, hat,2);
+var chordRow = rowCreator(335, chord,3);
+var chordTwoRow = rowCreator(435, chordTwo,4);
+var bassRow = rowCreator(535, bass,5);
+var stab = rowCreator(635, stab,6);
 
+var muteRect = new Path.Rectangle({
+			point: [1245,35],
+			size: [50,80.9],
+			fillColor: '#31D843',
+			strokeColor: '#E1F2FE',
+			strokeWidth: 3,
+			muteToggle: false,
+			sound: kick,
+			mute: function(){
+					for(var i = 0; i < sounds.length; i++){
+						
+						if (sounds[i].row === 0){
+							sounds[i].sound.mute([true]);
+							this.fillColor = '#BCC1BA'
+						}
+					}
+				},
+			unmute: function(){
+					for(var i = 0; i < sounds.length; i++){
+						if (sounds[i].row === 0){
+							sounds[i].sound.mute([false]);
+							this.fillColor = '#31D843'
+						}
+					}
+				},
+		})
+
+muteRect.onClick = function(e){
+	if (this.muteToggle){
+		this.unmute();
+		this.muteToggle = false;
+	} else{this.mute();
+		this.muteToggle = true;
+	}
+}
 
 	function onKeyDown(event){
 
@@ -80,6 +198,19 @@ var chordRow = rowCreator(335, chord);
 		}		
 		else if (event.key === 'n'){
 			chordFour.play();
+		} else if (event.key === 'p'){
+			playState = !playState;
+
+			if(playState){
+				play();
+			} else {
+				stop();
+				currentCol = 0;
+			}
+
 		}
 	};
 console.log('we out here')
+
+
+
